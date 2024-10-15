a3e5619c8344c0166783ee7fcb31f5cef27558d2
Martin Raszyk <martin.raszyk@dfinity.org>
2024-10-15 23:22:31 +0200

    .

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn test() {
+    use pocket_ic::PocketIcBuilder;
+    use std::str::FromStr;
+    let pic = PocketIcBuilder::new().with_server_url(reqwest::Url::from_str("http://localhost:8000").unwrap()).with_nns_subnet().build();
+}
