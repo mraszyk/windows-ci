d7aa7b1b912aca970ba0b751dda09dfe43395cfd
Martin Raszyk <martin.raszyk@dfinity.org>
2024-10-16 18:52:31 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ fn test() {
     use std::str::FromStr;
     let server_host = std::fs::read_to_string("host.txt").unwrap();
     let server_port = std::fs::read_to_string("port.txt").unwrap();
+    println!("host: {}", server_host);
+    println!("port: {}", server_port);
     let pic = PocketIcBuilder::new()
         .with_server_url(
             reqwest::Url::from_str(&format!("http://{}:{}", server_host, server_port)).unwrap(),
