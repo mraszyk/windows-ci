eb0693be1801401940d463f225e5160fb8db8c5b
Martin Raszyk <martin.raszyk@dfinity.org>
2024-10-16 14:32:54 +0200

    .

--- x.sh
+++
@@ -1,1 +0,0 @@
-sudo apt update && sudo apt install -y wget curl && wget https://download.dfinity.systems/ic/4bd8e1d8430ee17c7e3be47732d1786074ecb592/binaries/x86_64-linux/pocket-ic.gz && gzip -d pocket-ic.gz && chmod +x pocket-ic && ./pocket-ic --ip-addr 0.0.0.0 --port 8057 --ttl 2592000
