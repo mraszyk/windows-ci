606212700c683fdad75517f7014efc9e5db5d8f7
Martin Raszyk <martin.raszyk@dfinity.org>
2024-10-17 07:21:42 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,8 @@ fn main() {
 fn test() {
     use pocket_ic::PocketIcBuilder;
     use std::str::FromStr;
-    let server_host = std::fs::read_to_string("host.txt").unwrap();
-    let server_port = std::fs::read_to_string("port.txt").unwrap();
-    println!("host: {}", server_host);
-    println!("port: {}", server_port);
     let pic = PocketIcBuilder::new()
-        .with_server_url(
-            reqwest::Url::from_str(&format!("http://{}:{}", server_host, server_port)).unwrap(),
-        )
+        .with_server_url(reqwest::Url::from_str("http://localhost:8000").unwrap())
         .with_nns_subnet()
         .build();
     let _canister_id = pic.create_canister();
